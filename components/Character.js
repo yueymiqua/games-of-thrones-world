20cc6ee118fa5a6c3db033d7c19ec7b0cb445226
yueymiqua <pierre1116@gmail.com>
2021-06-28 01:16:13 -0400

    update character.js with more displayed info

--- components/Character.js
+++ components/Character.js
@@ -6,7 +6,14 @@ const Character = ({character}) => {
             <h2>{character.characterName}</h2>
             <h3>{character.houseName}</h3>
             <img src={character.characterImageThumb}/>
-            <br />
+            {character.killedBy === undefined ?
+                null :
+                <h3>Killed By: {character.killedBy}</h3>
+            }
+            {character.killed === undefined ?
+                null :
+                character.killed.map(murdered => <h3>Has Killed: {murdered}</h3>)
+            }
         </div>
     )
 }
